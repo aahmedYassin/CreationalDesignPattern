4b135ea1e5aa952ffde84ba218fa4193db0445f3
Ahmed Yassin <yassinahmed670@gmail.com>
2023-04-20 15:06:34 +0200

    patterns code

---
+++ src/main/java/gov/iti/jets/creationaldesignpattern/factory/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gov.iti.jets.creationaldesignpattern;
+
+/**
+ *
+ * @author Ahmed Yassin
+ */
+public class CreationalDesignPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
